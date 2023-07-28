01d3e01c531217dd3d40ef22b61958acf2365703
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-07-28 23:07:57 +0530

    Added Basic Filter Options

--- src/components/productpage/ProductCollection.tsx
+++ src/components/productpage/ProductCollection.tsx
@@ -11,9 +11,15 @@ interface Product {
   rating: number;
   brand: string;
 }
+interface selectedFiltersInterface {
+  [key: string]: boolean;
+}
 
 const ProductCollection = () => {
   const [productCollection, setProductCollection] = useState<Product[]>();
+  const [selectedFilters, setSelectedFilters] =
+    useState<selectedFiltersInterface>({});
+
   const numberOfProduct = 10;
   const generateProduct = (count: number): Product[] => {
     const products: Product[] = [];
@@ -28,7 +34,7 @@ const ProductCollection = () => {
         }),
         price: faker.commerce.price(),
         rating: Math.floor(Math.random() * 5) + 1,
-        brand: i % 2 === 0 ? "Mango" : "H&M",
+        brand: i % 2 === 0 ? "Mango" : "HM",
       };
       products.push(product);
     }
@@ -36,14 +42,60 @@ const ProductCollection = () => {
     return products;
   };
 
-  const getFilterHandler = () => {};
+  const getFilterHandler = (value: selectedFiltersInterface) => {
+    setSelectedFilters(value);
+  };
 
   useEffect(() => {
     const generatedProduct = generateProduct(numberOfProduct);
     setProductCollection(generatedProduct);
   }, []);
-  console.log("sam");
+
   console.log(productCollection);
+
+  const applyFilters = (
+    products: Product[],
+    filters: selectedFiltersInterface
+  ): Product[] => {
+    if (Object.keys(filters).length === 0) {
+      return products;
+    }
+
+    return products.filter((product) => {
+      if (+product.price <= 500 === filters.und500) {
+        return true;
+      }
+      if (+product.price > 500 === filters.abv500und3000) {
+        return true;
+      }
+      if ((product.brand === "Mango") === filters.mango) {
+        return true;
+      }
+      if ((product.brand === "HM") === filters.hm) {
+        return true;
+      }
+      if ((product.rating === 1) === filters.onestar) {
+        return true;
+      }
+      if ((product.rating === 2) === filters.twostar) {
+        return true;
+      }
+      if ((product.rating === 3) === filters.threestar) {
+        return true;
+      }
+      if ((product.rating === 4) === filters.fourstar) {
+        return true;
+      }
+      if ((product.rating === 5) === filters.star) {
+        return true;
+      }
+
+      return false;
+    });
+  };
+
+  const filteredProducts = applyFilters(productCollection, selectedFilters);
+
   return (
     <div className="flex flex-row ">
       <div className="md:w-[20%]">
@@ -51,7 +103,7 @@ const ProductCollection = () => {
       </div>
 
       <div className="grid grid-cols-4 md:w-[80%]">
-        {productCollection?.map((product) => (
+        {filteredProducts?.map((product) => (
           <ProductElement key={product.id} product={product} />
         ))}
       </div>
--- src/components/productpage/ProductElement.tsx
+++ src/components/productpage/ProductElement.tsx
@@ -1,43 +1,36 @@
-import React from 'react'
-import {AiFillStar} from 'react-icons/ai'
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
 
 interface Product {
-    id: number;
-    name: string;
-    image: string;
-    price: string;
-    rating:number
-  }
-  
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  rating: number;
+}
 
 interface ProductElementProps {
- product : Product
+  product: Product;
 }
 
-const ProductElement : React.FC<ProductElementProps>  = ({product}) => {
-   
-    const renderStars = () => {
-        const stars = [];
-        for (let i = 0; i < product.rating; i++) {
-          stars.push(<AiFillStar key={i} size={30} />);
-        }
-        return stars;
-      };
-        
+const ProductElement: React.FC<ProductElementProps> = ({ product }) => {
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 0; i < product.rating; i++) {
+      stars.push(<AiFillStar key={i} size={30} />);
+    }
+    return stars;
+  };
+
   return (
     <div>
-        <img src={product.image} alt={product.name} className='w-56 h-48'  />
-        <h1>
-            {product.name}
-        </h1>
-        <h3>{product.price}</h3>
-        <div className='flex space-x-2'>
-            
-        {renderStars()}
-        </div>
-         
-    </div>
-  )
-}
+      <img src={product.image} alt={product.name} className="w-56 h-48" />
 
-export default ProductElement
+      <h1>{product.name}</h1>
+      <h3>{product.price}</h3>
+      <div className="flex space-x-2">{renderStars()}</div>
+    </div>
+  );
+};
+
+export default ProductElement;
