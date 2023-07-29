899b8ad9547e3c26ce43fdc972d5bc64cba92693
“sujitmemane” <officialsujitmemane@gmail.comgit config --global user.name sujitmemanegit config --global user.email officialsujitmemane@gmail.com>
2023-07-29 12:49:07 +0530

    Organized the Code and Made it Maintanable

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import SearchBar from './home/SearchBar'
+import SuggestionBox from './home/SuggestionBox'
+import ProductCollection from './productpage/ProductCollection'
+
+export { SearchBar, SuggestionBox, ProductCollection }
